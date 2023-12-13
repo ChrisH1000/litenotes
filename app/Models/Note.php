b68f468287e24e5e075a000bca205b13315c0f7d
Chris Hill <christopher.hill@ecu.edu.au>
2023-12-13 13:39:06 +0800

    added relationships between user and notes

--- app/Models/Note.php
+++ app/Models/Note.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
 
 class Note extends Model
 {
@@ -14,5 +15,9 @@ class Note extends Model
     public function getRouteKeyName()
     {
         return 'uuid';
+    }
+
+    public function user() {
+        return $this->belongsTo(User::class);
     }
 }
