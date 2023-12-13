2a1893e2ab17daa7193a8304ab3c16152bf80fdf
Chris Hill <christopher.hill@ecu.edu.au>
2023-12-13 14:46:42 +0800

    new contraller for trashed notes

---
+++ app/Http/Controllers/TrashedNoteController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Note;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+
+class TrashedNoteController extends Controller
+{
+    public function index() {
+        $notes = Note::whereBelongsTo(Auth::user())->onlyTrashed()->latest('updated_at')->paginate(5);
+        return view('notes.index')->with('notes', $notes);
+    }
+
+    public function show(Note $note) {
+        if(!$note->user->is(Auth::user())) {
+            abort(403, 'Unauthorized action.');
+        }
+
+        return view('notes.show')->with('note', $note);
+    }
+}
