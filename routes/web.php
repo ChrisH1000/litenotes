e6a5b7a7aa8f5361ebfdd0cbf1e72f75a6bf652e
Chris Hill <christopher.hill@ecu.edu.au>
2023-12-13 15:06:29 +0800

    fixed route prefix

--- routes/web.php
+++ routes/web.php
@@ -43,8 +43,8 @@ Route::delete('/trashed/{note}', [TrashedNoteController::class, 'destroy'])->wit
 Route::prefix('/trashed')->name('trashed.')->middleware(['auth'])->group(function () {
     Route::get('/', [TrashedNoteController::class, 'index'])->name('index');
     Route::get('/{note}', [TrashedNoteController::class, 'show'])->name('show')->withTrashed();
-    Route::put('/{note}', [TrashedNoteController::class, 'update'])->name('index')->withTrashed();
-    Route::delete('/{note}', [TrashedNoteController::class, 'destroy'])->name('index')->withTrashed();
-}
+    Route::put('/{note}', [TrashedNoteController::class, 'update'])->name('update')->withTrashed();
+    Route::delete('/{note}', [TrashedNoteController::class, 'destroy'])->name('destroy')->withTrashed();
+});
 
 require __DIR__.'/auth.php';
